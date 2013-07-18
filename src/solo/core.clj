580b7446681d3e2eb3b495cfa7dffee18163a94a
peter-fogg <peter.fogg@oberlin.edu>
2013-07-18 07:29:25 -0400

    Rework to more elegantly handle failure; add `chain-parser`.

--- src/solo/core.clj
+++ src/solo/core.clj
@@ -10,8 +10,7 @@
   "Parse a single character."
   [state]
   (if (empty? state)
-    {:val ""
-     :rest ""}
+    nil
     {:val (first state)
      :rest (rest state)}))
 
@@ -30,7 +29,7 @@
   "Map `f` across the result of `parser`."
   [f parser]
   (fn [state]
-    (let [result (parser state)]
+    (when-let [result (parser state)]
       (assoc result :val (f (:val result))))))
 
 ;; Let's see an example of using map-parser. First, we'll define
@@ -39,18 +38,27 @@
   "Return a parser consuming as long as `pred` is true."
   [pred]
   (fn [state]
-    {:val (apply str (take-while pred state))
-     :rest (apply str (drop-while pred state))}))
+    (let [val (apply str (take-while pred state))
+          rest (apply str (drop-while pred state))]
+      (if (empty? val)
+        nil
+        {:val val
+         :rest rest}))))
 
 ;; Now we'll use parse-while for a quite useful purpose: parsing
 ;; numbers. For now we'll only parse positive integers; we'll fix that
 ;; later.
+(def parse-int
+  (map-parser read-string (parse-while #(some #{%} "0123456789"))))
 
-;; Now, the real deal.
-(defn parse-int
-  "Parse an integer."
-  [state]
-  ((map-parser read-string
-               (parse-while
-                #(some #{%} "0123456789")))
-   state))
+;; We won't always be parsing integers. It's good to have some way to
+;; run one parser, then the next (and possibly the next!). Chaining
+;; parsers allows us to do this, making the value of one parser
+;; available to the next and updating the state correctly.
+(defn chain-parser
+  "Combine two parsers into a new one."
+  [left right]
+  (fn [state]
+    (let [{val :val new-state :rest} (left state)]
+      (when val
+        ((right val) new-state)))))
