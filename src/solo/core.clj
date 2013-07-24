1971c6cb19140d8080479c0365728d3dd2f8a86e
peter-fogg <peter.fogg@oberlin.edu>
2013-07-24 19:42:41 -0400

    Remove all that literate programming-esque nonsense.

--- src/solo/core.clj
+++ src/solo/core.clj
@@ -1,22 +1,5 @@
 (ns solo.core)
 
-;; We'll represent a parse state as a map of {:val value, :rest
-;; string}. A parser is then a function of (string -> parse
-;; state). Those of you familiar with Haskell may recognize this as a
-;; monad. It is!
-
-;; An example of a parser. How exciting!
-(comment
-  (defn parse-char
-    "Parse a single character."
-    [state]
-    (if (empty? state)
-      nil
-      {:val (first state)
-       :rest (apply str (rest state))})))
-
-;; We can do better, though -- our above implementation of parse-char
-;; isn't very general. Let's abstract a bit.
 (defn satisfy
   "Returns a parser accepting a character satisfying `pred`."
   [pred]
@@ -26,11 +9,8 @@
         {:val c
          :rest (apply str (rest state))}))))
 
-;; Let's use satisfy to implement parse-char.
 (def parse-char (satisfy (fn [_] true)))
 
-;; The constant parser simply returns a parser that always gives the
-;; same value. Does this sound familiar?
 (defn constant
   "The constant parser."
   [val]
@@ -38,8 +18,6 @@
     {:val val
      :rest state}))
 
-;; It's often useful to apply some function to the result of a
-;; parser. Let's do that:
 (defn map-parser
   "Map `f` across the result of `parser`."
   [f parser]
@@ -47,8 +25,6 @@
     (when-let [result (parser state)]
       (assoc result :val (f (:val result))))))
 
-;; Let's see an example of using map-parser. First, we'll define
-;; another useful function, parse-while.
 (defn parse-while
   "Return a parser consuming as long as `pred` is true."
   [pred]
@@ -60,16 +36,9 @@
         {:val val
          :rest rest}))))
 
-;; Now we'll use parse-while for a quite useful purpose: parsing
-;; numbers. For now we'll only parse positive integers; we'll fix that
-;; later.
 (def parse-int
   (map-parser read-string (parse-while #(some #{%} "0123456789"))))
 
-;; We won't always be parsing integers. It's good to have some way to
-;; run one parser, then the next (and possibly the next!). Chaining
-;; parsers allows us to do this, making the value of one parser
-;; available to the next and updating the state correctly.
 (defn chain-parser
   "Combine two parsers into a new one."
   [left right]
@@ -77,8 +46,6 @@
     (when-let [{val :val new-state :rest} (left state)]
       ((right val) new-state))))
 
-;; We can use the state of our parser with chain-parser, but these two
-;; functions allow us to directly access and modify it.
 (defn get-state
   "Get the current state of the parser."
   [state]
@@ -90,9 +57,6 @@
   (fn [_]
     {:val '() :rest state}))
 
-;; Using chain-parser can get tedious and error-prone, what with all
-;; the nested functions. We define a macro, parse, which cleans up the
-;; process for us.
 (defmacro parse [& body]
   (cond
    (nil? body) '()
@@ -106,23 +70,17 @@
                  `(fn ~param
                     ~`(parse ~@(rest body)))))))
 
-;; This function allows us to expect a single character, and fail if
-;; it isn't in our string to parse.
 (defn expect-char
   "Expect `char`, and fail if not present."
   [char]
   (satisfy (partial = char)))
 
-;; What if we have a choice of values to parse? parse-or lets us try
-;; one parser, then the next, then the next, and so on.
 (defn parse-or
   "Choice of several parsers."
   [& parsers]
   (fn [state]
     (some #(% state) parsers)))
 
-;; The parse-many combinator allows us to collect any number of a certain
-;; parser, and return a list of what we find.
 (defn parse-many
   "Repeatedly apply `parser`."
   [parser]
@@ -133,15 +91,11 @@
       (constant (apply str (cons val rest))))
      state)))
 
-;; We can use one-of to give us a choice between any of a number of
-;; characters.
 (defn one-of
   "Parse any of the supplied characters."
   [chars]
   (satisfy #(some (partial = %) chars)))
 
-;; If we want to assert that a character is not present in our string,
-;; we can use not-char. It is the opposite of expect-char.
 (defn not-char
   "Reject the supplied character."
   [char]
@@ -150,15 +104,11 @@
       {:val (str (first state))
        :rest (apply str (rest state))})))
 
-;; none-of is the just opposite of one-of -- it ensures that it does
-;; not see any of the given characters.
 (defn none-of
   "Reject any of the supplied characters."
   [chars]
   (satisfy #(not (some (partial = %) chars))))
 
-;; If we want to parse a whole string (rather than just a character),
-;; we can do so with parse-string.
 (defn parse-string
   "Parse the given string."
   [string]
@@ -169,8 +119,6 @@
      (constant (apply str c rest)))
     (constant '())))
 
-;; sep-by allows us to separate a string by a particular parser. This
-;; makes it easy to, say, split up a sentence by whitespace.
 (defn sep-by
   "Parse a list of `left`, separated by `right`."
   [left right]
