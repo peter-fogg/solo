09224858393ff8c1bcb65f3ef6e3629844a534be
peter-fogg <peter.fogg@oberlin.edu>
2013-07-27 12:06:05 -0400

    JSON parser improvements.
    Some code cleanup and allowing spaces between tokens.

--- doc/examples/json.clj
+++ doc/examples/json.clj
@@ -6,28 +6,11 @@
 
 (declare parse-json)
 
-(def test-object "{\"foo\": 3, \"bar\": {\"pies\": [3.4,   2]}}")
+(def test-object "{\"foo\" : 3 , \"bar\": {\"pies\": [3.4,   2 ]} }")
 
 (def spaces
   "Maybe parse lots of whitespace."
   (parse-maybe (parse-many whitespace)))
-
-(def comma
-  "The separator for objects and arrays."
-  (parse
-   spaces
-   (expect-char \,)
-   spaces))
-
-(def json-object-field
-  "Parse a single field (key: value) of a JSON object. We'll represent
-   keys (usually strings) as Clojure keywords."
-  (parse
-   [key (parse-string-literal \")]
-   (expect-char \:)
-   spaces
-   [value parse-json]
-   (constant [(keyword key) value])))
 
 (defn wrap-spaces
   "Parse parse, possibly surrounded by whitespace."
@@ -38,13 +21,26 @@
    spaces
    (constant string)))
 
+(def comma
+  "The separator for objects and arrays."
+  (wrap-spaces (expect-char \,)))
+
+(def json-object-field
+  "Parse a single field (key: value) of a JSON object. We'll represent
+   keys (usually strings) as Clojure keywords."
+  (parse
+   [key (parse-string-literal \")]
+   (wrap-spaces (expect-char \:))
+   [value parse-json]
+   (constant [(keyword key) value])))
+
 (def json-object
   "Parse the whole JSON object."
-  (parse
-   [items (parse-delimited
-           \{ \}
-           (wrap-spaces (sep-by json-object-field comma)))]
-   (constant (into {} items))))
+  (map-parser
+   #(into {} %)
+   (parse-delimited
+    \{ \}
+    (wrap-spaces (sep-by json-object-field comma)))))
 
 (def json-array
   "Parse a JSON array."
